5c8a8ef7198c3f189a3eb9b468039cde1a41b02d
Thaer Hendawi <thaerhindawi55@gmail.com>
2023-03-02 16:15:29 -0500

    Updates

---
+++ precache-manifest.d3cc390c81b2a47b2ad646bf61210ca4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "3013971b3b7aa22f814b",
+    "url": "/my-app/static/css/main.26014f45.chunk.css"
+  },
+  {
+    "revision": "3013971b3b7aa22f814b",
+    "url": "/my-app/static/js/main.3013971b.chunk.js"
+  },
+  {
+    "revision": "071f6974c376b039043f",
+    "url": "/my-app/static/js/1.071f6974.chunk.js"
+  },
+  {
+    "revision": "a15ddd4fbed747e548c1",
+    "url": "/my-app/static/js/2.a15ddd4f.chunk.js"
+  },
+  {
+    "revision": "e625cd893e69147e2edf",
+    "url": "/my-app/static/js/runtime~main.e625cd89.js"
+  },
+  {
+    "revision": "cf89e8e6daa9dabc8174c303e4d53d3a",
+    "url": "/my-app/static/media/default_profile_image.cf89e8e6.png"
+  },
+  {
+    "revision": "1484d78ac6285e03df41fd337e03af54",
+    "url": "/my-app/static/media/moviesapp.1484d78a.png"
+  },
+  {
+    "revision": "a5c416d98e86f2d8e7fe40d05d2dd4a3",
+    "url": "/my-app/static/media/logo-imdb.a5c416d9.svg"
+  },
+  {
+    "revision": "8875700212e9e7749ecaa99b3e10089f",
+    "url": "/my-app/static/media/background.88757002.jpg"
+  },
+  {
+    "revision": "2d9c25992da25d86264c80c6fc021191",
+    "url": "/my-app/index.html"
+  }
+];
--- src/components/UI/Error/Error.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Error(props) {
-    return <div className="error">{props.error}</div>
-}
